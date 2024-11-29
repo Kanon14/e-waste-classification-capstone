a650e9e76cff1d35245ea45a2df14797e350f0b8
Kanon14 <geochua144@gmail.com>
2024-11-29 18:52:10 +0800

    update data download link

--- ewasteDetection/constant/training_pipeline/__init__.py
+++ ewasteDetection/constant/training_pipeline/__init__.py
@@ -7,7 +7,7 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_DIR: str = "https://drive.google.com/file/d/1qqYj4DHFdvef-fwhbUem_qfIdVlQ4Byv/view?usp=drive_link"
+DATA_DOWNLOAD_DIR: str = "https://drive.google.com/file/d/1lyK0JzBT0FZRze4P5nNJA-m7nHpwL9Ky/view?usp=drive_link"
 
 
 """
